36bbf3a3a20e460b722fd3c519f98f1abe8b8ea2
sadic <254041851+sadicintaste@users.noreply.github.com>
2026-03-08 23:22:52 +0800

    feat(index): api

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!-- tcg -->
+<?php
+
+// adjust based on your directory
+require_once '../PHP/vendor/autoload.php';
+
+// suppress deprecated warnings
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+use TCGdex\TCGdex;
+
+$tcgdex = new TCGdex("en");
+
+$cardsResponse = $tcgdex->card->list();
+$randomKeys = array_rand($cardsResponse, 5);
+$displayCards = [];
+
+foreach ($randomKeys as $key) {
+    $displayCards[] = $tcgdex->card->get($cardsResponse[$key]->id);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,48 +50,27 @@ include "inc/head.inc.php";
             <div class="container">
                 <div class="row justify-content-center gap-3">
 
-                    <div class="index-card" onclick="indexFlip(this)">
-                        <div class="index-card-inner">
-                            <div class="index-card-front"></div>
-                            <div class="index-card-back bg-primary"></div>
-                        </div>
-                    </div>
+                    <?php foreach ($displayCards as $card): ?>
+                        <div class="index-card" onclick="indexFlip(this)">
+                            <div class="index-card-inner">
+                                <div class="index-card-front">
+                                    <img src="assets/img/pokemon-card-back.png" alt="" class="img-fluid">
+                                </div>
 
-                    <div class="index-card" onclick="indexFlip(this)">
-                        <div class="index-card-inner">
-                            <div class="index-card-front"></div>
-                            <div class="index-card-back bg-danger"></div>
+                                <div class="index-card-back">
+                                    <img src="<?php echo $card->image . '/high.png'; ?>" alt="<?php echo $card->name; ?>" class="img-fluid" style="border-radius: 12px;">
+                                </div>
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="index-card" onclick="indexFlip(this)">
-                        <div class="index-card-inner">
-                            <div class="index-card-front"></div>
-                            <div class="index-card-back bg-success"></div>
-                        </div>
-                    </div>
-
-                    <div class="index-card" onclick="indexFlip(this)">
-                        <div class="index-card-inner">
-                            <div class="index-card-front"></div>
-                            <div class="index-card-back bg-warning"></div>
-                        </div>
-                    </div>
-
-                    <div class="index-card" onclick="indexFlip(this)">
-                        <div class="index-card-inner">
-                            <div class="index-card-front"></div>
-                            <div class="index-card-back bg-info"></div>
-                        </div>
-                    </div>
+                    <?php endforeach; ?>
                 </div>
 
                 <!-- register -->
                 <div id="index-register" class="text-center mt-4 d-none opacity-0">
                     <p class="text-white mb-3">Love your deck?</p>
-                        <a href="signup.php">
-                            <button class="btn btn-outline-light btn-lg">Register to Save</button>
-                        </a>
+                    <a href="signup.php">
+                        <button class="btn btn-outline-light btn-lg">Register to Save</button>
+                    </a>
                 </div>
 
             </div>
