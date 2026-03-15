7733821319a33e31535c3b584dd979d09c8a8063
Onglai0610 <jolynoyq@gmail.com>
2026-03-15 20:51:10 +0800

    nav: add sign up and sign out icons based on user authentication status
    add signout_process.php to handle user sign out

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -36,16 +36,30 @@ $isIn = isset($_SESSION['user_id']);
                 <button class="btn btn-success" type="submit">Search</button>
             </form>
             <ul class="navbar-nav">
+                
+                // Show sign up icon only if user is not signed in
+                <?php if (!$isIn): ?>
                 <li class="nav-item">
                     <a href="signup.php" class="nav-link">
                         <img src="images/register_icon.png" alt="Sign Up" title="Sign Up" class="nav-icon" />
                     </a>
                 </li>
+                <?php endif; ?>
+
                 <li class="nav-item">
                     <a href="<?php echo $isIn ? 'account.php' : 'signin.php'; ?>" class="nav-link">
                         <img src="images/login_icon.png" alt="<?php echo $isIn ? 'Account' : 'Sign In'; ?>" title="<?php echo $isIn ? 'Account' : 'Sign In'; ?>" class="nav-icon" />
                     </a>
                 </li>
+
+                // Show sign out icon only if user is signed in
+                <?php if ($isIn): ?>
+                <li class="nav-item">
+                    <a href="signout_process.php" class="nav-link">
+                        <img src="images/logout_icon.png" alt="Sign Out" title="Sign Out" class="nav-icon" />
+                    </a>
+                </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
